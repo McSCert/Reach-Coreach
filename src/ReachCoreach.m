30810f7f16a5dc4665bb4aa62d961e6364751335
Bennett Mackenzie <mackeb@mcmaster.ca>
2016-07-05 15:48:42 +0000

    more changes regarding coreach structure being implemented
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/ReachCoreach@7337 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/ReachCoreach.m
+++ src/ReachCoreach.m
@@ -40,7 +40,9 @@ classdef ReachCoreach < handle
         
         function slice(object)
             %remove all blocks other than the reached/coreached blocks
-            
+            allObjects=find_system(object.RootSystemName, 'FindAll', 'On', 'type', 'line');
+            allObjects=[allObjects find_system(object.RootSystemName, 'FindAll', 'On', 'type', 'block')];
+            toDelete=setdiff(allObjects, object.ReachedObjects);
         end
         
         function clear(object)
@@ -52,9 +54,13 @@ classdef ReachCoreach < handle
         function reachAll(object, selection)
             %get all the outports from the selected blocks
             for i=1:length(selection)
-                object.ReachedObjects(end+1)=get_param(selection{i}, 'handle');
-                ports=get_param(selection{i}, 'PortHandles');
-                object.PortsToTraverse=[object.PortsToTraverse ports.Outport];
+                if strcmp(get_param(selection{i}, 'BlockType'), 'SubSystem')
+                    %???
+                else
+                    object.ReachedObjects(end+1)=get_param(selection{i}, 'handle');
+                    ports=get_param(selection{i}, 'PortHandles');
+                    object.PortsToTraverse=[object.PortsToTraverse ports.Outport];
+                end
             end
             %reach from each in the list of ports to traverse
             while ~isempty(object.PortsToTraverse)
@@ -65,6 +71,24 @@ classdef ReachCoreach < handle
             object.hiliteObjects();
         end
         
+        function coreachAll(object, selection)
+            for i=1:length(selection)
+                if strcmp(get_param(selection{i}, 'BlockType'), 'SubSystem')
+                    %???
+                else
+                    object.CoreachedObjects(end+1)=get_param(selection{i}, 'handle');
+                    ports=get_param(selection{i}, 'PortHandles');
+                    object.PortsToTraverseCo=[object.PortsToTraverseCo ports.Inport];
+                end
+            end
+            %coreach from each in the list of ports to traverse
+            while ~isempty(object.PortsToTraverseCo)
+                port=object.PortsToTraverseCo(end);
+                object.PortsToTraverseCo(end)=[];
+                coreach(object, port)
+            end
+            object.hiliteObjects();
+        end
         function reach(object, port)
             %check if this port was already traversed
             if isempty(setdiff(port, object.TraversedPorts))
@@ -127,24 +151,46 @@ classdef ReachCoreach < handle
                             object.PortsToTraverse(end+1)=port;
                         end
                         
-                    case 'WhileIterator'
-                        %get errything
+                    case {'WhileIterator', 'ForIterator'}
+                        %get all blocks/ports in the subsystem, then reach
+                        %the blocks the outports, gotos, and writes connect
+                        %to outside of the system.
                         blocks=find_system(object.RootSystemName, 'LookUnderMasks', 'all', 'FollowLinks', 'on');
+                        object.ReachedObjects=[object.ReachedObjects getSimulinkBlockHandle(blocks)];
                         ports=find_system(object.RootSystemName, 'LookUnderMasks', 'all', 'FollowLinks', 'on');
-                    case 'BusSelector'
-                        blockLines=get_param(block, 'LineHandles');
-                        blockLines=blockLines.Outport;
-                        nextLines=get_param(nextBlocks(i), 'LineHandles');
-                        nextLines=nextLines.Inport;
-                        line=intersect(blockLines, nextLines);
-                        signalName=get_param(line, 'Name');
-                        if ~isempty(signalName)
-                            dstPort=get_param(line, 'DstPortHandle');
-                            portNum=get_param(dstPort, 'PortNumber');
-                            dest=traverseBusForwards(nextBlocks(i), ['signal' num2str(portNumber)]);
-                        else
-                            dest=traverseBusForwards(nextBlocks(i), signalName);
+                        object.TraversedPorts=[object.TraversedPorts ports];
+                        outports=find_system(system, 'SearchDepth', 1, 'BlockType', 'Outport');
+                        for j=1:length(outports)
+                            portNum=get_param(outports{j}, 'Port');
+                            parent=get_param(outports{j}, 'parent');
+                            if ~isempty(get_param(parent, 'parent'))
+                                port=find_system(get_param(parent, 'parent'), 'SearchDepth', 1, 'FindAll', 'on', ...
+                                    'type', 'port', 'parent', parent, 'PortType', 'outport', 'PortNumber', str2num(portNum));
+                                object.ReachedObjects(end+1)=get_param(parent, 'handle');
+                                object.PortsToTraverse(end+1)=port;
+                            end
                         end
+                        gotos=find_system(system, 'BlockType', 'Goto');
+                        for j=1:length(gotos)
+                            froms=findFromsInScope(gotos{j});
+                            for k=1:length(froms)
+                                object.ReachedObjects(end+1)=get_param(froms{k}, 'handle');
+                                outport=get_param(froms{k}, 'PortHandles');
+                                outport=outport.Outport;
+                                object.PortsToTraverse(end+1)=outport;
+                            end
+                        end
+                        writes=find_system(system, 'BlockType', 'DataStoreWrite');
+                        for j=1:length(writes)
+                            reads=findDataStoreReads(writes{j});
+                            for k=1:length(reads)
+                                object.ReachedObjects(end+1)=reads(k);
+                                outport=get_param(reads(k), 'PortHandles');
+                                outport=outport.Outport;
+                                object.PortsToTraverse(end+1)=outport;
+                            end
+                        end
+
                     case 'BusCreator'
                         blockLines=get_param(block, 'LineHandles');
                         blockLines=blockLines.Outport;
@@ -195,28 +241,70 @@ classdef ReachCoreach < handle
             end
         end
         
-        function source=traverseBusBackwards(block, signal)
-            portConnectivity=get_param(block, 'PortConnectivity');
-            srcBlocks=portConnectivity.SrcBlock;
-            next=srcBlocks(1);
-            blockType=get_param(next, 'BlockType');
-            switch blockType
-                case 'BusSelector'
-                    
-                case 'BusCreator'
-                    
-                case 'From'
-                    
-                case 'SubSystem'
-                    
-                case 'Inport'
-                    
-                otherwise
-                    
+        function coreach(object, port)
+            %check if this port was already traversed
+            if isempty(setdiff(port, object.TraversedPortsCo))
+                return
+            end
+            
+            %get block port belongs to
+            block=get_param(port, 'parent');
+            
+            %mark this port as traversed
+            object.TraversedPortsCo(end+1)=port;
+            
+            %get line from the port, and then get the destination blocks
+            line=get_param(port, 'line');
+            object.CoreachedObjects(end+1)=line;
+            nextBlocks=get_param(line, 'SrcBlockHandle');
+            
+            for i=1:length(nextBlocks)
+                %add block to list of coreached objects
+                object.CoreachedObjects(end+1)=nextBlocks(i);
+                %get blocktype for switch case
+                blockType=get_param(nextBlocks(i), 'BlockType');
+                %switch statement that handles the reaching of blocks
+                %differently.
+                switch blockType
+                    case 'From'
+                        gotos=findGotosInScope(nextBlocks(i));
+                        for j=1:length(gotos)
+                            object.CoreachedObjects(end+1)=get_param(gotos{j}, 'handle');
+                            inport=get_param(gotos{j}, 'PortHandles');
+                            inport=inport.Inport;
+                            object.PortsToTraverseCo(end+1)=inport;
+                        end
+                    case 'DataStoreRead'
+                        writes=findDataStoreWrites(nextBlocks{i});
+                        for j=1:length(writes)
+                            object.CoreachedObjects(end+1)=writes(j);
+                            inport=get_param(writes(j), 'PortHandles');
+                            inport=inport.Inport;
+                            object.PortsToTraverseCo(end+1)=inport;
+                        end
+                    case 'SubSystem'
+                        srcPorts=get_param(line, 'SrcPortHandle');
+                        for j=1:length(srcPorts)
+                            portNum=get_param(srcPorts(j), 'PortNumber');
+                            outport=find_system(nextBlocks(i), 'BlockType', 'Outport', 'Port', num2str(portNum));
+                            object.CoreachedObjects(end+1)=get_param(outport, 'Handle');
+                            inport=get_param(outport, 'PortHandles');
+                            inport=inport.Outport;
+                            object.PortsToTraverseCo(end+1)=inport;
+                        end
+                    case 'Inport'
+                        
+                    case {'WhileIterator', 'ForIterator'}
+                        
+                    case 'BusSelector'
+                        
+                    case 'If'
+                        
+                    otherwise
+                        
+                        
+                end
             end
         end
-    end
-
-
 end
 
