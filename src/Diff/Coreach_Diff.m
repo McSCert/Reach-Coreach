8c78c166fda04fdc305b7f6f29b0b80a03e78bba
Gordon Marks <gor.marks@gmail.com>
2020-01-22 00:08:47 +0000

    Added option to pass the diff tree from matlab's model differencing tool as an argument (to save time if calling repeatedly for the same pair of models).
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/ReachCoreach@19698 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/Diff/Coreach_Diff.m
+++ src/Diff/Coreach_Diff.m
@@ -1,4 +1,4 @@
-function [oldCoreachedObjects, newCoreachedObjects] = Coreach_Diff(oldModel, newModel, direction)
+function [oldCoreachedObjects, newCoreachedObjects] = Coreach_Diff(oldModel, newModel, direction, diffTree)
     % COREACH_DIFF Identifies blocks and lines in oldModel and newModel that
     % potentially impact the components changed between the models.
     % 
@@ -7,6 +7,9 @@ function [oldCoreachedObjects, newCoreachedObjects] = Coreach_Diff(oldModel, new
     %   newModel    The new version of a model.
     %   direction   Indicates direction of analysis. Default: 1 for upstream
     %               analysis (Coreach), 0 for downstream analysis (Reach).
+    %   diffTree    [Optional] Result of:
+    %                   slxmlcomp.compare(oldModel,newModel)
+    %               Only used to speed up results.
     %
     % Outputs:
     %   oldCoreachedObjects Handles of blocks and lines in oldModel that 
@@ -17,6 +20,11 @@ function [oldCoreachedObjects, newCoreachedObjects] = Coreach_Diff(oldModel, new
     
     if nargin < 3
         direction = 1; % Upstream trace (Coreach).
+    end
+    
+    % Get comparison tree.
+    if nargin < 4
+        diffTree = slxmlcomp.compare(oldModel, newModel);
     end
     
     [oldCoreachedObjects, newCoreachedObjects] = Reach_Diff(oldModel, newModel, direction);
--- src/Diff/Reach_Diff.m
+++ src/Diff/Reach_Diff.m
@@ -1,4 +1,4 @@
-function [oldReachedObjects, newReachedObjects] = Reach_Diff(oldModel, newModel, direction)
+function [oldReachedObjects, newReachedObjects] = Reach_Diff(oldModel, newModel, direction, diffTree)
     % REACH_DIFF Identifies blocks and lines in oldModel and newModel that are 
     % potentially impacted by the changes made between the models.
     % 
@@ -7,6 +7,9 @@ function [oldReachedObjects, newReachedObjects] = Reach_Diff(oldModel, newModel,
     %   newModel    The new version of a model.
     %   direction   Indicates direction of analysis. Default: 0 for downstream
     %               analysis (Reach), 1 for upstream analysis (Coreach).
+    %   diffTree    [Optional] Result of:
+    %                   slxmlcomp.compare(oldModel,newModel)
+    %               Only used to speed up results.
     %
     % Outputs:
     %   oldReachedObjects   Handles of blocks and lines in oldModel that are
@@ -17,6 +20,11 @@ function [oldReachedObjects, newReachedObjects] = Reach_Diff(oldModel, newModel,
     
     if nargin < 3
         direction = 0; % Downstream trace (Reach).
+    end
+    
+    % Get comparison tree.
+    if nargin < 4
+        diffTree = slxmlcomp.compare(oldModel, newModel);
     end
     
     % Load models.
@@ -34,7 +42,7 @@ function [oldReachedObjects, newReachedObjects] = Reach_Diff(oldModel, newModel,
     end
     
     % Get differences.
-    [oldBlocks, oldLines, newBlocks, newLines] = get_diffs_for_reachcoreach(oldModel, newModel);
+    [oldBlocks, oldLines, newBlocks, newLines] = get_diffs_for_reachcoreach(oldModel, newModel, diffTree);
     
     % Get the reaches.
     oldReachedObjects = getReach(oldModel, oldBlocks, oldLines, direction);
@@ -62,32 +70,4 @@ function reachedObjs = getReach(model, blocks, lines, direction)
         reachObj.coreachAll(blocks, lines);
         reachedObjs = reachObj.CoreachedObjects;
     end
-end
-
-function [oldBlocks, oldLines, newBlocks, newLines] = get_diffs_for_reachcoreach(model1, model2)
-    
-    % Note: The implementation here does not look for changes within stateflow.
-    
-    % Compare models.
-    diffStruct = model_diff(model1, model2);
-    
-    % The changes that we need for Reach/Coreach are blocks, lines, and
-    % lines connected to changed ports.
-    oldBlocks = unique([diffStruct.blocks.del.old, diffStruct.blocks.mod0.old]);
-    
-    newBlocks = unique([diffStruct.blocks.mod0.new, diffStruct.blocks.add.new]);
-    
-    oldLines = unique([diffStruct.lines.del.old, diffStruct.lines.mod0.old, ...
-        get_port_lines(diffStruct.ports.del.old), ...
-        get_port_lines(diffStruct.ports.mod0.old)]);
-    
-    newLines = unique([diffStruct.lines.add.new, diffStruct.lines.mod0.new, ...
-        get_port_lines(diffStruct.ports.add.new), ...
-        get_port_lines(diffStruct.ports.mod0.new)]);
-end
-
-function lines = get_port_lines(ports)
-    % Get lines connected to given ports.
-    lines = arrayfun(@(ph) get_param(ph, 'Line'), ports);
-    lines = lines(arrayfun(@(lh) lh ~= -1, lines));
 end
